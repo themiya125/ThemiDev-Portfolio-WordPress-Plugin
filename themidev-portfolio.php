330fb5e75dd7daf683d6f4a1c8d5382ff3240a87
Themiya Jayakodi <themiyajayakody125@gmail.com>
2026-02-08 11:21:19 +0530

    Update themidev-portfolio.php

--- themidev-portfolio.php
+++ themidev-portfolio.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ThemiDev Portfolio
  * Description: Portfolio projects manager 
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: Themiya Jayakodi
  * License: GPL v2 or later
  * Author URI: https://themidev.com/
